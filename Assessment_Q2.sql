5c038d23c32de72aeeb4fe59e43ada2d3a6492f7
Lawal Idris Tomiwa <olatomiwal679@gmail.com>
2025-06-15 02:56:40 +0100

    Update Assessment_Q2.sql

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -6,27 +6,35 @@ Query to Calculate the average number of transactions per customer per month and
 */
 
 -- Categorise Customers Frequency based on monthly transactions
-WITH frequency AS (SELECT owner_id, name, Monthly_avg,
-		CASE 
-		WHEN Monthly_avg >= 10 THEN 'High Frequency'
-		WHEN Monthly_avg >= 3 AND Monthly_avg <=9 THEN 'Medium Frequency'
-		WHEN Monthly_avg <= 2 THEN 'Low Frequency'
-        ELSE 'Unspecified Frequency'
-		END AS frequency_category
+WITH frequency AS (
+		SELECT owner_id, 
+			name, 
+			Monthly_avg,
+			CASE 
+				WHEN Monthly_avg >= 10 THEN 'High Frequency'
+				WHEN Monthly_avg >= 3 AND Monthly_avg <=9 THEN 'Medium Frequency'
+				WHEN Monthly_avg <= 2 THEN 'Low Frequency'
+		        ELSE 'Unspecified Frequency'
+			END AS frequency_category
 FROM (
-		SELECT owner_id, name, count(*)/count(distinct transaction_month_year) AS Monthly_avg
+		SELECT owner_id, 
+			name, 
+			count(*)/count(distinct transaction_month_year) AS Monthly_avg
 		FROM (
-				SELECT owner_id, CONCAT(u.first_name, ' ', u.last_name) AS name, CONCAT(MONTH(transaction_date), ',', YEAR(transaction_date)) AS transaction_month_year, YEAR(transaction_date) AS transaction_year
-				FROM savings_savingsaccount s
-				INNER JOIN users_customuser u
-						ON u.id = s.owner_id
-				WHERE u.is_active = 1
+			SELECT owner_id, 
+				CONCAT(u.first_name, ' ', u.last_name) AS name, 
+				CONCAT(MONTH(transaction_date), ',', YEAR(transaction_date)) AS transaction_month_year, 
+				YEAR(transaction_date) AS transaction_year
+			FROM savings_savingsaccount s
+			INNER JOIN users_customuser u
+				ON u.id = s.owner_id
+			WHERE u.is_active = 1
 ) s GROUP BY owner_id, name -- Get Average monthly transactions for active customer
 ) a
 )
 
 SELECT frequency_category, 
-		COUNT(DISTINCT name) AS customer_count, -- Count Distinct name instead of id to avoid customer duplication
+	COUNT(DISTINCT name) AS customer_count, -- Count Distinct name instead of id to avoid customer duplication
         AVG(Monthly_avg) AS avg_transactions_per_month
 FROM frequency
 GROUP BY frequency_category;
